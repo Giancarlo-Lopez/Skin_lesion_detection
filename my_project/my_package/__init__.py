799f2b3412a21994ab36e41a17c1b2f2955568a7
Giancarlo <105015743+Giancarlo-Lopez@users.noreply.github.com>
2024-06-03 21:54:43 -0400

    Delete my_project directory

--- my_project/my_package/__init__.py
+++
@@ -1,9 +0,0 @@
-from .class_asymmetry import ClassAsymmetry
-from .class_border import ClassBorder
-from .class_color import ClassColor
-from .class_diameter import ClassDiameter
-from .class_for_operations_masks import ClassForOperationsMasks
-from .class_for_plot import ClassForPlot
-from .class_segmentation_otsu import ClassSegmentationOtsu
-from .gui import GUI
-
